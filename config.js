7207db55b8641110613eab219b635df256c27d81
amagistrale <69706385+amagistrale@users.noreply.github.com>
2024-12-04 12:14:38 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+    "token": 'pk.eyJ1IjoiYW1hZ2lzdHJhbGUiLCJhIjoiY2xyanM3bTQ5MDFqMjJqcHdtem1uZmk3aiJ9.OLS4djSvledebuYH2fLFSg', //add your own MapBox Token
+    "style": "mapbox://styles/amagistrale/cltgq0i0f007301qo6xuz0yfa", // Change this URL to change the basemap style, this one is specifically for Fitzroy
+    "center": [-76.221, 45.482], // Change to center on different area upon opening
+    "zoom": 13.5, // change to zoom into different area upon opening
+    "sheeturl" : 'https://docs.google.com/spreadsheets/d/15XJHwzvbhwnOsn1EKw4tm8dfNjxnb9isd3Za6HbN-a0/edit#gid=645545594' // add your own google spreadsheet
+}
